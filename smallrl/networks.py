2036770cc556cb5b408d3cbfeb7aba711b43f68c
hello@nicklashansen.com <hello@nicklashansen.com>
2019-12-29 15:03:46 +0100

    added convolutional layers

--- smallrl/networks.py
+++ smallrl/networks.py
@@ -35,3 +35,47 @@ class TwoHeadedMLP(nn.Module):
     def forward(self, x):
         enc = F.relu(self.encoder(x))
         return self.policy(enc), self.value(enc)
+
+
+class CNN(nn.Module):
+    """
+    Convolutional neural network.
+    """
+    def __init__(self, channels, kernel_sizes, paddings):
+        super().__init__()
+        assert len(channels) > 1
+        assert len(kernel_sizes) == len(channels)-1
+        assert len(paddings) == len(channels)-1
+        layers = []
+        
+        for i in range(1, len(channels)):
+            layers.append(nn.Conv2d(
+                in_channels=channels[i-1],
+                out_channels=channels[i],
+                kernel_size=kernel_sizes[i-1],
+                padding=paddings[i-1])
+            )
+            if i != len(channels)-1: layers.append(nn.ReLU())
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class CNN_MLP(nn.Module):
+    """
+    Convolutional neural network with a fully connected output.
+    """
+    def __init__(self, channels, kernel_sizes, paddings, sizes):
+        super().__init__()
+
+        self.cnn = CNN(channels, kernel_sizes, paddings)
+        self.mlp = MLP(*sizes)
+
+
+    def forward(self, x):
+        x = self.cnn(x)
+        x = x.view(x.size(0), -1)
+        
+        return self.mlp(x)
